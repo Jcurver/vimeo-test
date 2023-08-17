01ce3259df1bb0c347c26a5cf8a6c7c505f7aa1d
Jcurver <pjh15237213@gmail.com>
2023-08-17 15:33:14 +0900

    clean code

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,13 @@ import Head from "next/head";
 import { useEffect, useRef, useState } from "react";
 import ReactPlayer from "react-player";
 import styles from "../styles/video.module.css";
+import ErrorBoundary from "./ErrorBoundary";
+import axios from "axios";
 
 const Home: NextPage = () => {
-  const [client, setClient] = useState(false);
-
-  const playerRef = useRef<ReactPlayer>(null);
   const [videoUrl, setVideoUrl] = useState("");
+  const [client, setClient] = useState(false);
+  const playerRef = useRef<ReactPlayer>(null);
   const [playing, setPlaying] = useState(false);
   const [isMuted, setIsMuted] = useState(false);
   const [playbackRate, setPlaybackRate] = useState(1);
@@ -16,54 +17,50 @@ const Home: NextPage = () => {
   const [currentTime, setCurrentTime] = useState(0);
   const [isVideoLoading, setIsVideoLoading] = useState(true);
 
+  const RNListener = (event: MessageEvent<any> | Event) => {
+    let parsedData: any = {};
+    //@ts-ignore
+    if (typeof event.data === "string") {
+      //@ts-ignore
+      parsedData = JSON.parse(event.data);
+    }
+
+    if (parsedData?.didimessage) {
+      const message = parsedData?.didimessage;
+      if (message?.videoUrl !== undefined) {
+        setVideoUrl(message.videoUrl);
+      }
+      if (message?.playing !== undefined) {
+        setPlaying(message.playing);
+      }
+      if (message?.isMuted !== undefined) {
+        setIsMuted(message.isMuted);
+      }
+      if (message?.playbackRate !== undefined) {
+        setPlaybackRate(message.playbackRate);
+      }
+      if (message?.currentTime !== undefined) {
+        if (message.currentTime > 1 && !duration) {
+          playerRef.current?.seekTo(message.currentTime - 1);
+        } else {
+          playerRef.current?.seekTo(message.currentTime);
+        }
+      }
+    }
+  };
+
   useEffect(() => {
     setClient(true);
   }, []);
 
   useEffect(() => {
-    window.addEventListener("message", (event) => {
-      let parsedData: any = {};
-      if (typeof event.data === "string") {
-        console.log("DD", event.data);
-
-        parsedData = JSON.parse(event.data);
-      } else {
-        // console.log("DD", event.data);
-      }
-
-      if (parsedData?.didimessage) {
-        const message = parsedData?.didimessage;
-        if (message?.videoUrl !== undefined) {
-          setVideoUrl(message.videoUrl);
-        }
-        if (message?.playing !== undefined) {
-          setPlaying(message.playing);
-        }
-        if (message?.isMuted !== undefined) {
-          setIsMuted(message.isMuted);
-        }
-        if (message?.playbackRate !== undefined) {
-          setPlaybackRate(message.playbackRate);
-        }
-        if (message?.currentTime !== undefined) {
-          if (message.currentTime > 1 && !duration) {
-            playerRef.current?.seekTo(message.currentTime - 1);
-          } else {
-            playerRef.current?.seekTo(message.currentTime);
-          }
-        }
-      }
-    });
+    window.addEventListener("message", (event) => RNListener(event));
+    document.addEventListener("message", (event) => RNListener(event));
   }, []);
 
   const messageToRN = (didimessage: string | object) => {
     //@ts-ignore
-    if (window.ReactNativeWebView) {
-      //@ts-ignore
-      window.ReactNativeWebView.postMessage(JSON.stringify({ didimessage }));
-    } else {
-      // alert({ message: "not mobile" });
-    }
+    window.ReactNativeWebView.postMessage(JSON.stringify({ didimessage }));
   };
 
   useEffect(() => {
@@ -95,50 +92,41 @@ const Home: NextPage = () => {
       </Head>
       <div style={{ width: "100%", height: "100%" }}>
         {client && (
-          <>
-            <ReactPlayer
-              ref={playerRef}
-              url={videoUrl}
-              playing={playing}
-              src={{}}
-              // url=""
-              width="100vw"
-              height="100vh"
-              playsinline
-              controls={false}
-              muted={isMuted}
-              playbackRate={playbackRate}
-              onDuration={(duration) => {
-                setDuration(duration);
-                console.log("duration", duration);
-              }}
-              onProgress={({ playedSeconds }) => setCurrentTime(playedSeconds)}
-              onReady={() => setIsVideoLoading(false)}
-              // options={{
-              //   fullscreen: {
-              //     iosNative: true,
-              //   },
-              // }}
-
-              config={{
-                vimeo: {
-                  playerOptions: {
-                    controls: false,
-                    responsive: true,
-                    muted: false,
-                    autopause: false,
-                    playsinline: true,
-                  },
+          <ReactPlayer
+            ref={playerRef}
+            url={videoUrl}
+            playing={playing}
+            width="100vw"
+            height="100vh"
+            playsinline
+            controls={false}
+            muted={isMuted}
+            playbackRate={playbackRate}
+            onDuration={(duration) => {
+              setDuration(duration);
+            }}
+            onProgress={({ playedSeconds }) => setCurrentTime(playedSeconds)}
+            onReady={() => setIsVideoLoading(false)}
+            config={{
+              vimeo: {
+                playerOptions: {
+                  controls: false,
+                  responsive: true,
+                  muted: false,
+                  autopause: false,
+                  playsinline: true,
+                  speed: true,
                 },
-                youtube: {
-                  playerVars: {
-                    controls: 0,
-                    modestbranding: 1,
-                  },
+              },
+              youtube: {
+                playerVars: {
+                  controls: 0,
+                  modestbranding: 1,
+                  showinfo: 0,
                 },
-              }}
-            />
-          </>
+              },
+            }}
+          />
         )}
       </div>
     </>
